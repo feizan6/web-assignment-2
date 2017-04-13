0b7e79d4c5e5a6d9eae83843c4ef21e4ae1e32e2
feizan6 <u1474090@unimail.hud.ac.uk>
2017-04-13 17:23:57 +0100

    created item model

---
+++ src/main/java/assignment2/Item.java
@@ -0,0 +1,33 @@
+package assignment2;
+
+/**
+ * Created by Fayzan on 13/04/2017.
+ */
+public class Item {
+    private String itemName;
+    private int quantity;
+    private double itemPrice;
+
+
+    public Item(String itemName, int quantity, double itemPrice){
+        this.itemName = itemName;
+        this.quantity = quantity;
+        this.itemPrice = itemPrice;
+    }
+
+    public double getItemPrice() {
+        return itemPrice;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public double getTotalPrice() {
+        return itemPrice * quantity;
+    }
+}
