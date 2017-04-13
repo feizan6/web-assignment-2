304ca3f80f62e2a1d3ac2c396c8806f1938bfce5
feizan6 <u1474090@unimail.hud.ac.uk>
2017-04-13 16:35:45 +0100

    Initial project skeleton

---
+++ README.md
@@ -0,0 +1,4 @@
+# Advanced Web Programming : Assignment 2
+Student ID : U1474090 <br />
+Student Name : Feizan Ulhaq <br />
+Framework being used : Spring <br />
---
+++ src/main/java/assignment2/ShoppingListManagerApplication.java
@@ -0,0 +1,12 @@
+package assignment2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingListManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingListManagerApplication.class, args);
+    }
+}
