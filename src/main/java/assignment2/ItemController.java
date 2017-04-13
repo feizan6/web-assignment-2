8ddfb05001ca511810d0cbd6e7b390d77ff9bc3a
feizan6 <u1474090@unimail.hud.ac.uk>
2017-04-13 17:41:13 +0100

    method created to show items within specified price range

--- src/main/java/assignment2/ItemController.java
+++ src/main/java/assignment2/ItemController.java
@@ -1,11 +1,13 @@
 package assignment2;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by Fayzan on 13/04/2017.
@@ -30,5 +32,12 @@ public class ItemController {
     public List<Item> getAll() {
 
         return items;
+    }
+
+    @RequestMapping(value = "/cheapest/{price}", method = RequestMethod.GET)
+    public List<Item> getItemsBelow(@PathVariable double price) {
+        return items.stream().filter(x -> x.getItemPrice() <= price)
+                .collect(Collectors.toList());
+
     }
 }
